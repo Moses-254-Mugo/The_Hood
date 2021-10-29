9ba91fd1dc7d5957e0916a11ca43d06d468f5c30
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-29 17:23:42 +0300

    Create tamplate base

---
+++ HoodApp/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+urlpatterns =[
+    # url('^$', views.welcome, name = 'welcome')
+    path('', views.welcome, name = 'welcome')
+
+]
