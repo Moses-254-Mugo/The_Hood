262c8bae9248b7e9d4601c9f5285c65b5e7e381f
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-30 16:23:08 +0300

    Create url path for update profile

--- HoodApp/urls.py
+++ HoodApp/urls.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from django.conf.urls import url
 from . import views
 from django.urls import path
@@ -5,7 +6,8 @@ urlpatterns =[
     # url('^$', views.welcome, name = 'welcome')
     path('', views.index, name = 'index'),
     path('my-profile/', views.myProfile, name='my-profile'),
-    path('create/profile', views.New_profile, name='new-profile')
+    path('create/profile', views.New_profile, name='new-profile'),
+    path('update/profile',views.update_profile, name='update_profile')
     # path('search/', views.search_results, name='search_results')t
 
 ]
