50372d7c39736d705a00ed6004566ec47d3359bb
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-30 10:25:06 +0300

    Load admin page and see new objects to edit on admin website

--- HoodApp/admin.py
+++ HoodApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Neighbourhood, Profile, Post, Police, Health, Business
+
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Police)
+admin.site.register(Health)
+admin.site.register(Business)
