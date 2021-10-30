6669950f955369d5cccda714c43fd15d98916b7c
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-30 10:00:11 +0300

    Import tinyme

--- HoodApp/models.py
+++ HoodApp/models.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
+from os import name
 from django.db import models
 from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Neighbourhood(models.Model):
@@ -21,11 +23,57 @@ class Neighbourhood(models.Model):
 
 class Profile(models.Model):
     image = CloudinaryField('image')
+    name = models.CharField(max_length=40)
+    description = HTMLField()
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.description
+
+class Post(models.Model):
+    post_img = CloudinaryField('image')
+    post = HTMLField()
+    title = models.CharField(max_length=200)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    username  = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
+
     
+class Business(models.Model):
+    name= models.CharField(max_length=150)
+    email = models.EmailField()
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    bus_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = HTMLField()
     
 
+    def __str__(self):
+        return self.name
+
+class Health(models.Model):
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    contact = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.address
 
 
+
+class Police(models.Model):
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    contact = models.IntegerField()
+    address = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
 
 
 
